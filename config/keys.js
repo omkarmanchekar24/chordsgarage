ee80d660d325667f10b60b6e361c83cb22d35200
Omkar Manchekar <omkarmanchekar.24@gmail.com>
2019-08-24 16:57:33 +0530

    Initial Commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://omkar24:1234@developer-24-0uilj.mongodb.net/garage?retryWrites=true&w=majority"
+    "mongodb+srv://omkar24:1234@developer-24-0uilj.mongodb.net/garage?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -12,10 +18,14 @@ const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log("MongoDB Connected Successfully..."))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Chordsgarage is cool"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 // Use Routes
 app.use("/api/users", users);
